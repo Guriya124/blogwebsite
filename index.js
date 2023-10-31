77896bf892030022b8fc0712e6d45e455ee7436b
Guriya124 <guriyakumarisgvu@gmail.com>
2023-10-31 15:25:41 +0530

    update the code

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-import express from "express";
-
+const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+const posts = [];
+
+app.get('/', (req, res) => {
+    const blogPosts = [
+        {
+            title: 'test',
+            createdDate: new Date(),
+            description: 'test description'
+        },
+        {
+            title: 'testn2',
+            createdDate: new Date(),
+            description: 'test description'
+        }
+
+    ]
 
 
+    res.render('index.ejs', { blogpost: blogPosts });
+});
 
-
+app.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
 
 
 
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
