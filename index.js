a5734720bf6242dfebd1177f6381bab46055cd85
Guriya124 <guriyakumarisgvu@gmail.com>
2023-11-05 15:00:23 +0530

    showing blogs on screen

--- index.js
+++ index.js
@@ -4,37 +4,36 @@ const port = 3000;
 const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-const posts = [];
+const blogPosts = [];
+
+
 
 app.get('/', (req, res) => {
-    const blogPosts = [
-        {
-            title: 'test',
-            createdDate: new Date(),
-            description: 'test description'
-        },
-        {
-            title: 'testn2',
-            createdDate: new Date(),
-            description: 'test description'
-        }
-
-    ]
-
-
     res.render('index.ejs', { blogpost: blogPosts });
 });
+
+
 
 app.get('/new', (req, res) => {
     res.render('new.ejs');
 });
 
+app.post('/create', (req, res) => {
 
+    const title = req.body.title;
+    const description = req.body.description;
+    const newPost = {
+        title: title,
+        createdDate: new Date(),
+        description: description
+    };
+    blogPosts.push(newPost);
+    res.redirect('/');
 
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
